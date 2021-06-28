12479b411836f71569acb752d6401d0f9c68167f
Chelsea <chelsea.senter10@gmail.com>
2021-06-28 15:54:58 -0400

    5e input completed, added lowercase, ADD logic/math, ADD results (with sum, max, min)

--- dice.py
+++ dice.py
@@ -2,15 +2,17 @@ import random
 
 run='y'
 while run == 'y':
+
+## introduction for user --------------------------------------------------------------------------------------------
     print(".")
     print(".")
     print(".")
     print(".")
     print(".")
     print(".")
-    print(" -------------------------------------------------------------------------------------------------------")
+    print(".-------------------------------------------------------------------------------------------------------*")
     print("| Welcome to the Dice Roller 5000! There are several different ways to roll dice. See below for details.|")
-    print(" -------------------------------------------------------------------------------------------------------")
+    print("*-------------------------------------------------------------------------------------------------------.")
     print(" ")
     print("_._._._._._._._._._._._._._._._._._._._._._")
     print(" ")
@@ -38,7 +40,12 @@ while run == 'y':
     print(".")
     print(".")
     print(".")
+
+## dice type --------------------------------------------------------------------------------------------
     dice_type = input("Which would you like to use? Type e for 5e Rolls, c for Custom Dice, or n to end this script:    ")
+    dice_type = dice_type.lower()
+
+## 5e Roller logic --------------------------------------------------------------------------------------------
     if dice_type == "e": 
         print("Type out your preferences using the following instructions:")
         print("1. decide the type of roll you want: i = _I_nitiative, c = skill _C_heck, t = saving _T_hrow, or a = _A_ttack roll")
@@ -47,9 +54,17 @@ while run == 'y':
         print("**check out these examples:**")
         print("      id+3 means rolling initiative at disadvantage adding a +3 modifier")
         print("      tn-1 means rolling a normal saving throw with a -1 modifier")
-        formula = input("type out your ")
-    else dice_type == "c":
+        try: 
+            formula = input("Enter your preferences here:   ")
+            formula = formula.lower()
+            print(formula)
+        finally:
+            run='n'
 
-    else dice_type == "n":
+## FUTURE: Custom Dice --------------------------------------------------------------------------------------------
+    else:
         run='n'
-    
+    # elif dice_type == "c":
+
+    # elif dice_type == "n":
+    #     run='n'
